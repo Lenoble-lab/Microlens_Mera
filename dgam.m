c838c6cf0377d546c6d04bbbc218e7b18f7544c2
Lenoble-lab <romain.lenoble@polytechnique.edu>
2020-04-18 17:03:10 +0200

    premiere lecture en entier

--- dgam.m
+++ dgam.m
@@ -4,7 +4,6 @@ global cosb cosbl sinb sinl siglr siglt siglp L0 Ro vlimit
 % mise en forme des inputs
 i = find(x<0 | x>1 | v < - vlimit | v > vlimit);
 
-[R, z, th] = toGC(x.*L); 
 px = rholens(x.*L).*sqrt(x.*(1-x));	% terme rho(xL)*sqrt(x(1-x))
 
 res = px.*v./sqrt(m).*L.^1.5;	% Calcul du dgamma (cf eq 3.25 p. 57 de ma these)
