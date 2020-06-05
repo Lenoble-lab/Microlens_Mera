56df1329b72b5ccfa8171629e5c3e1f7ff000e85
Lenoble-lab <romain.lenoble@polytechnique.edu>
2020-06-05 09:44:17 +0200

    ogleIV

--- exp_ogle_IV_2019.m
+++ exp_ogle_IV_2019.m
@@ -5,39 +5,126 @@
 % S. Kozlowski, J. Skowron, R. Poleski, K. Ulaczyk, M. Gromadzki,
 % K. Rybicki, P. Iwanek, and M. Wrona
 %---------------------------------------------------------------------------------------------------------------
-
+close all
+clear
 % Exposure ?
 
 
-ttobs=tau/(gam/1e6/365.25);
-disp(['<tobs> (en jours) = ' num2str(ttobs)]);
+% ttobs=tau/(gam/1e6/365.25);
+% disp(['<tobs> (en jours) = ' num2str(ttobs)]);
 % N=gam*exposure;
 % disp(['nb d''evt  = ' num2str(N)]);
 
-taur=gam*pi/2*uT*mean(te)/365.25/1e6;
-taur=real(taur);
-disp(['tau (avec gamma integré par MC) = ' num2str(taur)]);
+% taur=gam*pi/2*uT*mean(te)/365.25/1e6;
+% taur=real(taur);
+% disp(['tau (avec gamma integré par MC) = ' num2str(taur)]);
+
+%-----------------------------------------------
+%Table 6. Basic information about analyzed fields
+%------------------------------------------------
+delimiter = ' ';
+VarNames_table6 = {'field', 'ra', 'dec', 'glon', 'glat', 'N_stars', 'N_epochs'};
+VarTypes_table6 = {'string', 'double', 'double', 'double', 'double', 'double', 'double'}; 
+
+opts = delimitedTextImportOptions('VariableNames',VarNames_table6,'VariableTypes',VarTypes_table6,...
+                                'Delimiter',delimiter, 'DataLines', 18, ...
+                       'WhiteSpace', ' ', 'ConsecutiveDelimitersRule', 'join');
+table6 = readtable('../OGLEIV/table6.dat',opts);
+
+%-----------------------------------------------
+%Table 7. Microlensing optical depth and event rates in the OGLE-IV
+% fields (averaged over sources brighter than I=21).
+%-----------------------------------------------
+
+VarNames_table7 = {'field', 'glon', 'glat', 'tau', 'tau_err', 'gam', 'gam_err', 'gam_deg2',...
+    'gam_deg2_err', 't_E_mean', 't_E_mean_err', 'N_events', 'N_stars'};
+VarTypes_table7 = {'string', 'double', 'double', 'double', 'double', 'double', 'double', ...
+    'double', 'double', 'double', 'double', 'double', 'double'}; 
+
+opts = delimitedTextImportOptions('VariableNames',VarNames_table7,'VariableTypes',VarTypes_table7,...
+                                'Delimiter',delimiter, 'DataLines', 25, ...
+                       'WhiteSpace', ' ', 'ConsecutiveDelimitersRule', 'join');
+table7 = readtable('../OGLEIV/table7.dat',opts);
 
 
 %-----------------
-%Evenements
+%Table 3. Best-fitting parameters of the analyzed microlensing events in low-cadence OGLE fields.
 %-----------------
+varNames = {'name', 'field', 'star_id', 'ra', 'dec', 'ra', 'dec', 'glon',...
+    'glat', 't0_best', 'tE_best', 'u0_best', 'Is_best', 'fs_best', 't0_med', 't0_err1', 't0_err2', 'tE_med',...
+    'tE_err1', 'tE_err2', 'u0_med', 'u0_err1', 'u0_err2', 'Is_med', 'Is_err1', 'Is_err2', 'fs_med', 'fs_err1', 'fs_err2', 'weight', 'ews_id'} ;
+varTypes = {'char','string', 'int32' ,'char', 'char', 'double', 'double', 'double', 'double', ...
+    'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', ...
+    'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'char'} ;
 
-load ../OGLEIV/table3.dat
+opts = delimitedTextImportOptions('VariableNames',varNames,'VariableTypes',varTypes,...
+                                'Delimiter',delimiter, 'DataLines', 47, ...
+                       'WhiteSpace', ' ', 'ConsecutiveDelimitersRule', 'join');
+table3 = readtable('../OGLEIV/table3.dat',opts);
 
-ogle_field = table_B1(:,2);
-glon = table_B1(:,12)
-glat = table_B1(:,13);
-teff = table_B1(:,15);
+teff = table3.tE_best;
 
+nbre_bin =  300;
+bin_max = 300;
+[hist_teff, edges] = histcounts(teff, nbre_bin, 'BinLimits',[0,bin_max], 'BinMethod', 'sturges');
 
+centre = zeros(size(edges)-[0,1]);
+for j =1:length(centre);
+centre(j)=(edges(j)+edges(j+1))/2;
+end
+
+% figure(1)
+% loglog(centre, hist_teff)
+% range = 0:1e-1*2:1 * log(400);
+% bin_range = exp(range);
+% 
+% [bincounts] = histc(teff, bin_range);
+% figure(2)
+% loglog(bin_range, bincounts)
+
+%----------------
+%Tracé profondeur optique en fonction de la lattitude galactique au centre
+%---------------------
+long = 1;
+
+%exp ogle
+i0 = find(abs(table6.glon - long)<0.5 & table6.glat<0);
+
+%Calcul modèle
+tau_load = load('graph_iso_model.mat');
+
+[L, B] = meshgrid(tau_load.L, tau_load.B);
+
+i1 = find(tau_load.L==long);
+i2 = find(tau_load.B<0);
+
+figure(1)
+hold on
+errorbar(table6.glat(i0), table7.tau(i0), table7.tau_err(i0), 'o')
+plot(tau_load.B(i2), tau_load.tau_table(i1,i2)*1e6)
+legend('Mesure d''OGLE IV', 'Modèle')
+xlabel('b (deg)')
+ylabel('\tau \times 10^{-6}')
+%%
 %---------------
 %Efficacité
 %-----------
-load ../OGLEIV/eff
+VarNames = {'log_tE_min', 'log_tE_max', 'efficiency'};
+VarType = {'double', 'double', 'double'};
+
+opts = delimitedTextImportOptions('VariableNames',VarNames,'VariableTypes',VarType,...
+        'Delimiter',' ', 'DataLines', 4, 'WhiteSpace', ' ', 'ConsecutiveDelimitersRule', 'join');
+    
+% eff = readtable(strcat('../OGLEIV/eff/',extractBetween(table3.field(1), 1, 6),'.eff'),opts);
+eff = readtable(strcat('../OGLEIV/eff/','BLG500','.eff'),opts);
+
 %-----------------------------------------------------------------------------------------------
 % Interpolation lineaire de l'efficacite pour determiner la probabilite qu'un evt a d'etre garde
 %-----------------------------------------------------------------------------------------------
+
+figure(1)
+plot((10.^eff.log_tE_min + 10.^eff.log_tE_max)/2, eff.efficiency)
+
 
 [tinterpmacho,indice] = sort(tmacho2005); 
 effinterpmacho = effmacho2005t(indice) ;
