399b64acf530a2126420059004e57a4cea637d4e
Lenoble-lab <romain.lenoble@polytechnique.edu>
2020-07-07 11:04:01 +0200

    dgamma

---
+++ dgammax.m
@@ -0,0 +1,22 @@
+%Fonction pour calculer gammax au début du MC
+%Comme le calcul du max sur m et v est simple, on retire ces variables de
+%la fonction et on optimise uniquement sur L et x
+%On met en place la limitation sur dsup car sinon L tend vers +inf
+
+function res = dgammmax(data)
+x = data(1); L = data(2);
+
+global dsup vlimit minf
+
+if L<dsup
+res = -rholens(x.*L).*sqrt(x.*(1-x)).*L.^1.5.*vlimit/sqrt(minf);	% Calcul du dgamma (cf eq 3.25 p. 57 de ma these)
+else
+    res = 0;
+end
+    %pas de terme nsource(L) car les distances
+%sont tirees au hasard avec une loi proportionnelle
+%a nsource
+
+
+
+    
